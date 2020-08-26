23f23e1577301b307b52a88717d755804e4d77d4
gliech <gliech@users.noreply.github.com>
2020-08-26 12:20:24 +0200

    chore: lint

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const sodium = require('tweetsodium');
+const core = require("@actions/core")
+const github = require("@actions/github")
+const sodium = require("tweetsodium")
 
 class GithubLocation {
   constructor(location_input) {
-    this.type = 'repository'
-    this.short_type = 'Repo'
+    this.type = "repository"
+    this.short_type = "Repo"
     if (!location_input) {
-      const context = github.context;
-      this.data = context.repo;
-    } else if (location_input.contains('/')) {
-      const [owner, repo] = location_input.split('/')
+      const context = github.context
+      this.data = context.repo
+    } else if (location_input.contains("/")) {
+      const [owner, repo] = location_input.split("/")
       this.data = {owner, repo}
     } else {
-      this.type = 'organization'
-      this.short_type = 'Org'
+      this.type = "organization"
+      this.short_type = "Org"
       this.data = {org: location_input}
     }
   }
   toString() {
-    return Object.values(this.data).join('/')
+    return Object.values(this.data).join("/")
   }
 }
 
 async function run() {
   try {
     // Get all inputs
-    const input_name = core.getInput('name');
-    const input_value = core.getInput('value');
+    const input_name = core.getInput("name")
+    const input_value = core.getInput("value")
 
-    const input_location = core.getInput('location');
-    const secret_target = new GithubLocation(input_location);
+    const input_location = core.getInput("location")
+    const secret_target = new GithubLocation(input_location)
 
-    const input_pat = core.getInput('pa_token');
-    const octokit = github.getOctokit(input_pat);
+    const input_pat = core.getInput("pa_token")
+    const octokit = github.getOctokit(input_pat)
     const get_public_key = octokit.actions[`get${secret_target.short_type}PublicKey`]
     const upsert_secret = octokit.actions[`createOrUpdate${secret_target.short_type}Secret`]
 
-    if (secret_targe.type == 'organization') {
-      const input_visibility = core.getInput('org_visibility');
-      if (['all', 'private'].includes(input_visibility)) {
-        const org_arguments = { visibility: input_visibility }
+    let org_arguments = {}
+    if (secret_target.type == "organization") {
+      const input_visibility = core.getInput("org_visibility")
+      if (["all", "private"].includes(input_visibility)) {
+        org_arguments = { visibility: input_visibility }
       } else {
-        const org_arguments = {
-          visibility: 'selected',
-          selected_repositoy_ids: input_visibility.split(',').map(i => i.trim())
+        org_arguments = {
+          visibility: "selected",
+          selected_repositoy_ids: input_visibility.split(",").map(i => i.trim())
         }
       }
-    } else {
-      const org_arguments = {}
     }
+
     // Retrieve repository public key and encrypt secret value
     core.info(`Retrieving public key for ${secret_target.type} ${secret_target}`)
-    const { data: public_key } = await get_public_key(secret_target.data);
+    const { data: public_key } = await get_public_key(secret_target.data)
 
     core.info("Encrypting secret value")
-    const plain_value_bytes = Buffer.from(input_value);
-    const public_key_bytes = Buffer.from(public_key.key, 'base64');
-    const secret_value_bytes = sodium.seal(plain_value_bytes, public_key_bytes);
-    const signed_secret_value = Buffer.from(secret_value_bytes).toString('base64');
+    const plain_value_bytes = Buffer.from(input_value)
+    const public_key_bytes = Buffer.from(public_key.key, "base64")
+    const secret_value_bytes = sodium.seal(plain_value_bytes, public_key_bytes)
+    const signed_secret_value = Buffer.from(secret_value_bytes).toString("base64")
 
     // Create or update secret
     core.info(`Setting ${secret_target.type} secret "${secret_target}"`)
@@ -67,12 +67,12 @@ async function run() {
       secret_name: input_name,
       encrypted_value: signed_secret_value,
       key_id: public_key.key_id,
-      ...(secret_target.type == 'organization' && {visibility: input_visibility})
-    });
+      ...org_arguments
+    })
 
     const response_codes = {
-      201: 'created',
-      204: 'updated'
+      201: "created",
+      204: "updated"
     }
 
     if (status in response_codes) {
@@ -81,10 +81,10 @@ async function run() {
       core.warn
     }
 
-    core.setOutput("status", status);
+    core.setOutput("status", status)
   } catch (err) {
-    core.setFailed(err.message);
+    core.setFailed(err.message)
   }
 }
 
-run();
+run()
